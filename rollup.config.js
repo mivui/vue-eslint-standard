a524ba554a752575c762d8ede23426bca4a2699f
mivui <49021590+mivui@users.noreply.github.com>
2026-03-26 20:43:43 +0800

    config

--- rollup.config.js
+++ rollup.config.js
@@ -3,7 +3,7 @@ import typescript from '@rollup/plugin-typescript';
 import { defineConfig } from 'rollup';
 import { dts } from 'rollup-plugin-dts';
 
-const input = ['./lib/index.ts'];
+const input = ['./src/index.ts'];
 
 const plugins = [
   json({
